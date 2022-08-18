0eb628316a2037bd55f075317a6069a3a0bd7e47
nmotaung021 <nmotaung021@student.wethinkcode.co.za>
2022-08-18 11:16:34 +0200

    added views

--- website/Gallery/models.py
+++ website/Gallery/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class GalleryImages(models.Model):
+    image1 = models.ImageField(
+    upload_to="posts/bg/",
+    )
+    image2 = models.ImageField(
+    upload_to="posts/bg/",
+    )
+    image3 = models.ImageField(
+    upload_to="posts/bg/",
+    )
+    image4 = models.ImageField(
+    upload_to="posts/bg/",
+    )
+    image5 = models.ImageField(
+    upload_to="posts/bg/",
+    )
+    image6 = models.ImageField(
+    upload_to="posts/bg/",
+    )
+    image7 = models.ImageField(
+    upload_to="posts/bg/",
+    )
+    image8 = models.ImageField(
+    upload_to="posts/bg/",
+    )
+    image9 = models.ImageField(
+    upload_to="posts/bg/",
+    )
