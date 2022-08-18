4b916a950d753213982913cd2b627e26d6050d51
nmotaung021 <nmotaung021@student.wethinkcode.co.za>
2022-08-18 11:29:18 +0200

    deleted gallery app

--- website/index/views.py
+++ website/index/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from index.models import GalleryImages
+
+
 # Create your views here.
+def gallery(request):
+    image = GalleryImages.objects.all()
+    context = {
+        "image" : image,
+
+    }
+    
+    return render(request, "public/index.html", context)
